479139b1aac14bbc4f1d252d1af505142d1e1585
aBhAy_BiShT <75539409+AbHaY108BiShT@users.noreply.github.com>
2021-05-29 14:51:37 +0530

    Add files via upload

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    "login": "sunilnarwat",
+    "id": 78677398,
+    "node_id": "MDQ6VXNlcjc4Njc3Mzk4",
+    "avatar_url": "https://avatars.githubusercontent.com/u/78677398?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/sunilnarwat",
+    "html_url": "https://github.com/sunilnarwat",
+    "followers_url": "https://api.github.com/users/sunilnarwat/followers",
+    "following_url": "https://api.github.com/users/sunilnarwat/following{/other_user}",
+    "gists_url": "https://api.github.com/users/sunilnarwat/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/sunilnarwat/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/sunilnarwat/subscriptions",
+    "organizations_url": "https://api.github.com/users/sunilnarwat/orgs",
+    "repos_url": "https://api.github.com/users/sunilnarwat/repos",
+    "events_url": "https://api.github.com/users/sunilnarwat/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/sunilnarwat/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "Ash-Godson",
+    "id": 44782929,
+    "node_id": "MDQ6VXNlcjQ0NzgyOTI5",
+    "avatar_url": "https://avatars.githubusercontent.com/u/44782929?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Ash-Godson",
+    "html_url": "https://github.com/Ash-Godson",
+    "followers_url": "https://api.github.com/users/Ash-Godson/followers",
+    "following_url": "https://api.github.com/users/Ash-Godson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Ash-Godson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Ash-Godson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Ash-Godson/subscriptions",
+    "organizations_url": "https://api.github.com/users/Ash-Godson/orgs",
+    "repos_url": "https://api.github.com/users/Ash-Godson/repos",
+    "events_url": "https://api.github.com/users/Ash-Godson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Ash-Godson/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+]
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain='dev-lvwybrz1.us.auth0.com'
+      clientId='BFtfTmaM6ZCKcm5jiSZjjLUBAS76dfGw'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
